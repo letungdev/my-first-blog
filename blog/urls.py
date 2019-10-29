9c57b4d5a00d0c3e5b6685d2df2e866d8222981b
unknown <you@example.comle.tung.dev@gmail.com>
2019-10-29 18:06:52 +0700

    ok

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), 
+    path('about_us/',views.about_us, name = "about-us"),
+    path('home/',views.homepage, name = "home-page"),
+    path('blogs/',views.blogs, name = "blog-page"),
+    path('contact_us/',views.contact_us, name = "contact-page"),
+    path('course_detail/',views.course_detail, name = "details-page"),
+    path('course/',views.course_detail, name = "details-page"),
+    path('investories/',views.invest, name = "invest-page"),
+    path('students/',views.students, name = "students-page"),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,24 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from django.shortcuts import redirect
 
-# Create your views here.
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(request.POST, instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+	
+def about_us(request):
+	return render(request, "about_us.html")	
+def homepage(request):
+	return render(request, "blog/home.html")
+def blogs(request):
+	return render(request, "blog/blog.html")
+def contact_us(request):
+	return render(request, "blog/contact.html")
+def course_detail(request):
+	return render(request, "blog/course_details.html")
+def course(request):
+	return render(request, "blog/course.html")
+def students(request):
+	return render(request, "blog/students.html")
+def invest(request):
+	return render(request, "blog/invest.html")
